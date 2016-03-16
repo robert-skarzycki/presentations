5072bac6b6854b7c7a4d66fdeeafd7182deea687
Robert Skarżycki <robert.skarzycki@gmail.com>
2016-03-16 21:25:38 +0100

    calc library and poorman tests runner

---
+++ skarzycki.robert.unittests/skarzycki.robert.unittests.01CalcLibrary/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skarzycki.robert.unittests._01CalcLibrary
+{
+    public class Calculator
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Sum(params int[] elements)
+        {
+            return elements.Sum();
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ skarzycki.robert.unittests/skarzycki.robert.unittests.01CalcLibraryConsoleTests/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skarzycki.robert.unittests._01CalcLibraryConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new _01CalcLibrary.Calculator();
+
+            Console.WriteLine("Test 1: Suma dwóch składników");
+            var x = 10;
+            var y = 11;
+            var result = calculator.Sum(x, y);
+            if (result != x + y)
+                Console.Error.WriteLine("Niepoprawny wynik: {0} zamiast {1}", result, x + y);
+            else
+                Console.WriteLine("Test OK");
+
+            Console.ReadKey();
+        }
+    }
+}
